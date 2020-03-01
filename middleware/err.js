7b62235202a91b9b6d437bf7fcbaf938b81784cf
Ana Nikolova <ani.nik19@gmail.com>
2020-03-01 08:46:32 +0100

    slugify

--- middleware/err.js
+++ middleware/err.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
 
   error.message = err.message;
-  console.log(err);
+  //console.log(err);
 
   // Mongoose bad ObjectId
   if (err.name === "CastError") {
