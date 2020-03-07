30adae59b7879fa3c316f061fa9e904b3d592b12
Ana Nikolova <ani.nik19@gmail.com>
2020-03-07 10:08:17 +0100

    advanced results and middleware

--- controllers/courses.js
+++ controllers/courses.js
@@ -9,15 +9,15 @@ const ErrorResponse = require("../utils/errorResponse");
 //@acess Public
 
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find().populate({
-      path: "bootcamp",
-      select: "name description"
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+    return res.status(200).json({
+      sucess: true,
+      count: courses.length,
+      data: courses
     });
+  } else {
+    res.status(200).json(res.advancedResults);
   }
 
   const courses = await query;
