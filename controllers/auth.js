a087d5217b3285a6ffdaebfadeab43bdd12d5588
Ana Nikolova <ani.nik19@gmail.com>
2020-03-08 10:26:56 +0100

    sending jwt in a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,9 +17,11 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
 
   // Create token
-  const token = user.getSignedJwtToken();
+  // const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
+  // res.status(200).json({ success: true, token });
+
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc      Login user
@@ -47,8 +49,30 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
 
+  sendTokenResponse(user, 200, res);
+});
+
+// Fet token form model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      token
+    });
+};
